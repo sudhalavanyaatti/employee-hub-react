e4eacbde1a468abb10ce9668b18ac8c83f2c9c5d
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2019-07-24 11:22:38 +0530

    frontend

--- src/components/bottom.js
+++ src/components/bottom.js
@@ -9,14 +9,18 @@ class Bottom extends Component {
   render() {
     return ( 
          
-           <div className="btmnavbar">
-            <span id="footer">&copy; 2019 EmployeeHub.All Rights Reserved.</span>&nbsp;&nbsp;&nbsp;
-            <SocialIcon url="http://facebook.com" network="facebook" bgcolor="#FFFFFF" style={{ height: 25, width: 25 }} />
-            <SocialIcon url="http://twitter.com"  fgcolor="#ff6a01" style={{ height: 25, width: 25 }} />
-            <SocialIcon url="http://googleplus.com" style={{ height: 25, width: 25 }} />
-            <SocialIcon url="http://linkedin.com" style={{ height: 25, width: 25 }} />
-           </div>
-         
+      <div className="btmnavbar">
+      <div id="footer">
+      <h5 >&copy; 2019 EmployeeHub.All Rights Reserved.</h5>
+      </div>
+       <div id="hdr">
+       <SocialIcon url="http://facebook.com/in/jaketrent"   style={{  height: 25, width: 25 }} />
+       <SocialIcon url="http://twitter.com/in/jaketrent"  style={{ height: 25, width: 25 }} />
+       <SocialIcon url="http://googleplus.com/in/jaketrent" style={{ height: 25, width: 25 }} />
+       <SocialIcon url="http://linkedin.com/in/jaketrent" style={{ height: 25, width: 25}} />
+       </div>
+      </div>
+    
 
     );
   }
